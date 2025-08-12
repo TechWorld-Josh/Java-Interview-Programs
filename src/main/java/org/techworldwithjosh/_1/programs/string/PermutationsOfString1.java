40246264480b6b344e03cbb9e6c90808794e3ab5
Josh <josh@Joshs-MacBook-Pro.local>
2025-08-12 11:26:47 +0530

    some of new java program

--- src/main/java/org/techworldwithjosh/_1/programs/string/PermutationsOfString1.java
+++ src/main/java/org/techworldwithjosh/_1/programs/string/PermutationsOfString1.java
@@ -28,5 +28,27 @@ public class PermutationsOfString {
         String s = scan.next();
         permutation(s.toCharArray(), 0);
     }
+
+    private static class jdjdjdl {
+        public static void main(String[] args) {
+            String str = "ABC";
+            System.out.println("All permutations of " + str + ":");
+            generatePermutations(str, "");
+        }
+
+        // Recursive method
+        public static void generatePermutations(String str, String prefix) {
+            if (str.length() == 0) {
+                System.out.println(prefix);
+                return;
+            }
+
+            for (int i = 0; i < str.length(); i++) {
+                char currentChar = str.charAt(i);
+                String remaining = str.substring(0, i) + str.substring(i + 1);
+                generatePermutations(remaining, prefix + currentChar);
+            }
+        }
+    }
 }
 
